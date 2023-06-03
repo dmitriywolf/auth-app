1678d5e2f66f3e5d844b79365beaeb0e075ae5e0
Dmitriy Volchenko <dmitriyvolchenko3003@gmail.com>
2023-06-03 15:36:25 +0300

    trigger title

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
     <>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>Next Auth</title>
+        <title>Next Auth App</title>
       </Head>
       <SessionProvider session={session}>
         <ToastContainer
